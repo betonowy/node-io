3f42a1e16d1d92e9125a0b43699968f9e9a00dd8
betonowy <bl000dy@o2.pl>
2020-11-27 21:47:13 +0100

    dev

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const di = '[server.js] ';
 // dependencies
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(di + `Listening at http://localhost:${process.env.port}`);
-  console.info(di + 'this is info');
-  console.error(di + 'this is error');
-  console.debug(di + 'this is debug');
+  console.log(di + `Listening at https://localhost:${process.env.port}`);
 })
